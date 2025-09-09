969f8737527bed31c0dc198e776189491b8d343d
cobalt admin <nik@cobaltdata.net>
2025-09-09 17:59:22 +0000

    core

--- frontend/src/routes/_layout/hosting/index.tsx
+++ frontend/src/routes/_layout/hosting/index.tsx
@@ -1,4 +1,3 @@
-// src/routes/_layout/hosting/index.tsx
 import { createFileRoute, Link } from "@tanstack/react-router";
 import {
   Box,
@@ -19,8 +18,7 @@ import {
 } from "@chakra-ui/react";
 import { CopyIcon } from "@chakra-ui/icons";
 
-// Updated devices array with servers data
-interface Device {
+interface Server {
   name: string;
   email: string;
   ip: string;
@@ -31,147 +29,39 @@ interface Device {
   os: string;
   username: string;
   password: string;
-  monthlyComputePrice?: number;
-  storageSizeGB?: number;
-  activeSince?: string;
-  hasRotatingIP?: boolean;
-  hasBackup?: boolean;
-  hasMonitoring?: boolean;
+  monthlyComputePrice: number;
+  storageSizeGB: number;
+  activeSince: string;
+  hasRotatingIP: boolean;
+  hasBackup: boolean;
+  hasMonitoring: boolean;
   hasManagedSupport?: boolean;
   vCPUs?: number;
-  ramGB?: number;
+  ramGB: number;
 }
 
-const devices: Device[] = [
-  {
-    name: "e-coast-nyc-lower-8core-ssd",
-    email: "apis.popov@gmail.com",
-    ip: "100.100.95.59",
-    version: "1.82.0",
-    kernel: "Linux 6.8.0-57-generic",
-    status: "Connected",
-    type: "VPS",
-    os: "debian",
-    username: "user",
-    password: "5660",
-    monthlyComputePrice: 11.40,
-    storageSizeGB: 120,
-    activeSince: "2025-07-01",
-    hasRotatingIP: false,
-    hasBackup: true,
-    hasMonitoring: true,
-    hasManagedSupport: false,
-    vCPUs: 1,
-    ramGB: 2,
-  },
+const servers: Server[] = [
   {
     name: "e-coast-nyc-midtown-16core-ssd",
     email: "apis.popov@gmail.com",
-    ip: "100.140.50.60",
-    version: "1.88.0",
-    kernel: "Linux 6.8.0-62-generic",
+    ip: "100.120.30.40",
+    version: "1.85.0",
+    kernel: "Linux 6.8.0-60-generic",
     status: "Connected",
     type: "VPS",
-    os: "debian",
+    os: "ubuntu",
     username: "user",
     password: "5660",
-    monthlyComputePrice: 449,
-    storageSizeGB: 100,
-    activeSince: "2025-09-01",
-    hasRotatingIP: false,
-    hasBackup: false,
-    hasMonitoring: false,
-    hasManagedSupport: false,
-    vCPUs: 16,
+    monthlyComputePrice: 136.60,
+    storageSizeGB: 500,
+    activeSince: "2025-08-01",
+    hasRotatingIP: true,
+    hasBackup: true,
+    hasMonitoring: true,
     ramGB: 64,
-  },
-  {
-    name: "e-coast-nyc-bk-4core-hdd",
-    email: "apis.popov@gmail.com",
-    ip: "100.100.95.61",
-    version: "1.88.0",
-    kernel: "Linux 6.8.0-62-generic",
-    status: "Connected",
-    type: "VPS",
-    os: "debian",
-    username: "user",
-    password: "5660",
-    monthlyComputePrice: 40.1,
-    storageSizeGB: 468,
-    activeSince: "2025-09-01",
-    hasRotatingIP: false,
-    hasBackup: false,
-    hasMonitoring: false,
-    hasManagedSupport: false,
-    vCPUs: 4,
-    ramGB: 4,
-  },
-  {
-    name: "e-coast-jersey-jersey-4core-ssd",
-    email: "apis.popov@gmail.com",
-    ip: "100.100.95.62",
-    version: "1.88.0",
-    kernel: "Linux 6.8.0-62-generic",
-    status: "Connected",
-    type: "VPS",
-    os: "debian",
-    username: "user",
-    password: "5660",
-    monthlyComputePrice: 45.3,
-    storageSizeGB: 110,
-    activeSince: "2025-09-01",
-    hasRotatingIP: false,
-    hasBackup: false,
-    hasMonitoring: false,
-    hasManagedSupport: false,
-    vCPUs: 4,
-    ramGB: 16,
-  },
-  {
-    name: "e-coast-nyc-lower-8core-hdd",
-    email: "apis.popov@gmail.com",
-    ip: "100.100.95.63",
-    version: "1.88.0",
-    kernel: "Linux 6.8.0-62-generic",
-    status: "Connected",
-    type: "VPS",
-    os: "debian",
-    username: "user",
-    password: "5660",
-    monthlyComputePrice: 43.1,
-    storageSizeGB: 932,
-    activeSince: "2025-09-01",
-    hasRotatingIP: false,
-    hasBackup: false,
-    hasMonitoring: false,
-    hasManagedSupport: false,
-    vCPUs: 8,
-    ramGB: 4,
-  },
-  {
-    name: "e-coast-nyc-midtown-2core-ssd",
-    email: "apis.popov@gmail.com",
-    ip: "100.100.95.64",
-    version: "1.88.0",
-    kernel: "Linux 6.8.0-62-generic",
-    status: "Connected",
-    type: "VPS",
-    os: "debian",
-    username: "user",
-    password: "5660",
-    monthlyComputePrice: 40.1,
-    storageSizeGB: 240,
-    activeSince: "2025-09-01",
-    hasRotatingIP: false,
-    hasBackup: false,
-    hasMonitoring: false,
-    hasManagedSupport: false,
-    vCPUs: 2,
-    ramGB: 8,
   },
 ];
 
-// Reusable CopyCell component
 const CopyCell = ({ textToCopy, label }: { textToCopy: string; label: string }) => {
   const { onCopy } = useClipboard(textToCopy);
   const toast = useToast();
@@ -195,7 +85,7 @@ function HostingIndexPage() {
       <Flex align="center" py={6}>
         <Flex direction="column">
           <Text fontSize="3xl" color="black">VPS Credentials</Text>
-          <Text fontSize="lg" color="gray.600">Login details for hosting devices</Text>
+          <Text fontSize="lg" color="gray.600">Login details for hosting servers</Text>
         </Flex>
         <Button ml="auto" as={Link} to="billing">View Billing</Button>
       </Flex>
@@ -204,7 +94,7 @@ function HostingIndexPage() {
         <Table variant="simple" size="md">
           <Thead>
             <Tr>
-              <Th>Device Name</Th>
+              <Th>Server Name</Th>
               <Th>IP</Th>
               <Th>Username</Th>
               <Th>Password</Th>
@@ -212,17 +102,17 @@ function HostingIndexPage() {
             </Tr>
           </Thead>
           <Tbody>
-            {devices.map((device) => (
-              <Tr key={device.name}>
-                <Td>{device.name}</Td>
-                <Td>{device.ip}</Td>
-                <Td>{device.username}</Td>
-                <Td>{device.password}</Td>
+            {servers.map((server) => (
+              <Tr key={server.name}>
+                <Td>{server.name}</Td>
+                <Td>{server.ip}</Td>
+                <Td>{server.username}</Td>
+                <Td>****</Td>
                 <Td isNumeric>
                   <HStack spacing={2} justify="flex-end">
-                    <CopyCell textToCopy={device.username} label="Username" />
-                    <CopyCell textToCopy={device.password} label="Password" />
-                    <Button size="sm" as={Link} to={device.name}>View Details</Button>
+                    <CopyCell textToCopy={server.username} label="Username" />
+                    <CopyCell textToCopy={server.password} label="Password" />
+                    <Button size="sm" as={Link} to={server.name}>View Details</Button>
                   </HStack>
                 </Td>
               </Tr>
