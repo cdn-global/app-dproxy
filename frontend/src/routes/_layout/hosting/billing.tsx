d7782babbb08a624be3d78d9e1460de3ea04f05c
cobalt admin <nik@cobaltdata.net>
2025-09-10 02:35:41 +0000

    core

--- frontend/src/routes/_layout/hosting/billing.tsx
+++ frontend/src/routes/_layout/hosting/billing.tsx
@@ -557,10 +557,6 @@ function BillingPage() {
                       <Text fontWeight="bold">${grandTotal.toFixed(2)}</Text>
                     </Flex>
                     <Flex justify="space-between">
-                      <Text>Full Cost (Including Trials):</Text>
-                      <Text fontWeight="bold">${fullGrandTotal.toFixed(2)}</Text>
-                    </Flex>
-                    <Flex justify="space-between">
                       <Text>Invoiced Amount (September 2025):</Text>
                       <Text fontWeight="bold">${invoicedAmount.toFixed(2)}</Text>
                     </Flex>
