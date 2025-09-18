468e970bee3adf13d71452c08106298ceba51c5d
cobalt admin <nik@cobaltdata.net>
2025-09-18 10:06:41 +0000

    message

--- frontend/src/components/Common/Logo.tsx
+++ frontend/src/components/Common/Logo.tsx
@@ -12,23 +12,23 @@ const Logo: React.FC<LogoProps> = ({ href = '/', ...rest }) => {
           display="inline-flex"
           alignItems="center"
           justifyContent="center"
-          boxSize={{ base: 16, md: 26 }}
+          boxSize={{ base: 12, md: 26 }}
           borderRadius="lg"
           bgGradient="linear(to-br, orange.500, orange.300)"
           color="white"
           boxShadow="lg"
-          p={{ base: 3, md: 4 }}
+          p={{ base: 1.5, md: 4 }}
         >
           <Box position="relative" aria-hidden>
-            <Layers size={useBreakpointValue({ base: 18, md: 26 })} />
+            <Layers size={useBreakpointValue({ base: 14, md: 26 })} />
           </Box>
         </Box>
 
-        <Box lineHeight={1} color="gray.800" display={{ base: 'block', md: 'flex' }} alignItems="center">
-          <Text as="span" fontWeight="600" fontSize={{ base: 'xs', md: 'sm' }} display={{ base: 'block', md: 'inline' }} mr={{ base: 0, md: 1 }}>
+        <Box lineHeight={1} color="gray.800" display={{ base: 'flex', md: 'flex' }} alignItems="center">
+          <Text as="span" fontWeight="600" fontSize={{ base: 'xs', md: 'sm' }} display={{ base: 'inline', md: 'inline' }} mr={{ base: 1, md: 1 }}>
             DATA
           </Text>
-          <Text as="span" fontWeight="800" fontSize={{ base: 'sm', md: 'lg' }} display={{ base: 'block', md: 'inline' }} color="orange.500" letterSpacing="tighter">
+          <Text as="span" fontWeight="800" fontSize={{ base: 'sm', md: 'lg' }} display={{ base: 'inline', md: 'inline' }} color="orange.500" letterSpacing="tighter">
             PROXY
           </Text>
         </Box>
