b82ee55ea4b05dc6e7f74a35aad131a494182f0f
cobalt admin <nik@cobaltdata.net>
2025-09-18 09:45:39 +0000

    message

--- frontend/src/components/Common/Logo.tsx
+++ frontend/src/components/Common/Logo.tsx
@@ -7,35 +7,34 @@ interface LogoProps extends LinkProps {}
 const Logo: React.FC<LogoProps> = ({ href = '/', ...rest }) => {
   return (
     <Link href={href} _hover={{ textDecoration: 'none' }} {...rest}>
-      <Box
-        display="inline-flex"
-        alignItems="center"
-        px={{ base: 2, md: 3 }}
-        py={{ base: 1, md: 2 }}
-        borderRadius="lg"
-        bgGradient="linear(to-r, orange.400, orange.200)"
-        color="white"
-        boxShadow="md"
-      >
-        <HStack spacing={{ base: 2, md: 3 }} align="center">
-          <Box display="inline-flex" alignItems="center" justifyContent="center">
-            <Box position="relative" aria-hidden>
-              <Cloud size={useBreakpointValue({ base: 14, md: 18 })} strokeWidth={2.2} />
-              <Box position="absolute" right={-2} top={-1} boxSize={2} bg="whiteAlpha.700" borderRadius="full" />
-              <Box position="absolute" left={-2} bottom={0} boxSize={1.5} bg="whiteAlpha.600" borderRadius="full" />
-            </Box>
+      <HStack spacing={3} align="center">
+        <Box
+          display="inline-flex"
+          alignItems="center"
+          justifyContent="center"
+          boxSize={{ base: 8, md: 10 }}
+          borderRadius="md"
+          bgGradient="linear(to-br, orange.400, yellow.300)"
+          color="white"
+          boxShadow="sm"
+          p={1}
+        >
+          <Box position="relative" aria-hidden>
+            <Cloud size={useBreakpointValue({ base: 12, md: 16 })} strokeWidth={2.2} />
+            <Box position="absolute" right={-1} top={-1} boxSize={2} bg="whiteAlpha.700" borderRadius="full" />
+            <Box position="absolute" left={-1} bottom={0} boxSize={1.5} bg="whiteAlpha.600" borderRadius="full" />
           </Box>
+        </Box>
 
-          <Box lineHeight={1} color="white">
-            <Text as="span" fontWeight="500" fontSize={{ base: 'xs', md: 'sm' }} display="block">
-              DATA
-            </Text>
-            <Text as="span" fontWeight="700" fontSize={{ base: 'sm', md: 'md' }} display="block" letterSpacing="wide">
-              PROXY
-            </Text>
-          </Box>
-        </HStack>
-      </Box>
+        <Box lineHeight={1} color="gray.800">
+          <Text as="span" fontWeight="500" fontSize={{ base: 'xs', md: 'sm' }} display="block">
+            DATA
+          </Text>
+          <Text as="span" fontWeight="700" fontSize={{ base: 'sm', md: 'md' }} display="block" color="orange.500" letterSpacing="wide">
+            PROXY
+          </Text>
+        </Box>
+      </HStack>
     </Link>
   );
 };
