67eb71bc27bbb1ac7d39bc53ad049f8a7704cfc9
cobalt admin <nik@cobaltdata.net>
2025-09-18 09:39:06 +0000

    message

--- frontend/src/components/Common/Logo.tsx
+++ frontend/src/components/Common/Logo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, LinkProps, HStack, Box, Text } from '@chakra-ui/react';
-import { Layers, Database } from 'react-feather';
+import { Database } from 'react-feather';
 
 interface LogoProps extends LinkProps {}
 
@@ -21,21 +21,18 @@ const Logo: React.FC<LogoProps> = ({ href = '/', ...rest }) => {
           justifyContent="center"
           boxSize={10}
           borderRadius="md"
-          bgGradient="linear(to-br, orange.400, yellow.300)"
+          bgGradient="linear(to-br, orange.500, orange.300)"
           color="white"
           boxShadow="sm"
         >
-          <Box position="relative">
-            <Layers size={18} strokeWidth={2.2} />
-            <Database size={10} strokeWidth={2} style={{ position: 'absolute', right: -6, bottom: -6, opacity: 0.9 }} />
-          </Box>
+          <Database size={20} strokeWidth={2} />
         </Box>
 
         <Box lineHeight={1}>
-          <Text as="span" fontWeight="700" fontSize="lg" color="gray.800" display="block">
+          <Text as="span" fontWeight="600" fontSize="sm" color="gray.700" display="block">
             DATA
           </Text>
-          <Text as="span" fontWeight="600" fontSize="sm" color="orange.400" display="block" letterSpacing="wide">
+          <Text as="span" fontWeight="800" fontSize="lg" color="orange.500" display="block" letterSpacing="wide">
             PROXY
           </Text>
         </Box>
